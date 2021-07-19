6c852343798b20e202e881acdf80ebdb100ec851
harin yoo <u.halynn@gmail.com>
2021-07-19 18:28:35 +0900

    기본 사용법

---
+++ Python/09_Container/1.py
@@ -0,0 +1,10 @@
+print(type('brooklyn'))
+name = 'brooklyn'
+print(name)
+print(type(['brooklyn', 'sydney', 'victoria']))
+names = ['brooklyn', 'sydney', 'victoria']
+print(names)
+print(names[1])
+brooklyn = ['data scientist', 'new york', 30, True]
+brooklyn[1] = 'manhattan'
+print(brooklyn)

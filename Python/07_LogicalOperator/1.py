43de3e464857bf02d308542124fdedc6c3c2957c
harin yoo <u.halynn@gmail.com>
2021-07-19 16:05:40 +0900

    OR

---
+++ Python/07_LogicalOperator/1.py
@@ -0,0 +1,7 @@
+in_str = input("아이디를 입력해주세요.\n")
+real_brooklyn = "brooklyn"
+real_sydney = "sydney"
+if real_brooklyn == in_str or real_sydney == in_str:
+    print("Hello!")
+else:
+    print("Who are you?")

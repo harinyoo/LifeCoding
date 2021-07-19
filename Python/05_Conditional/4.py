b472eeb78536f36912ad7a5767ef21febe54c029
harin yoo <u.halynn@gmail.com>
2021-07-19 16:02:08 +0900

    else if

---
+++ Python/05_Conditional/4.py
@@ -0,0 +1,9 @@
+input = 22
+real_brooklyn = 11
+real_sydney = 22
+if real_brooklyn == input:
+    print("Hello! brooklyn")
+elif real_sydney == input:
+    print("Hello! sydney")
+else:
+    print("Who are you?")

cd6767d3b699b2a2e2b63c0fd2e26bfe3c8f9c83
harin yoo <u.halynn@gmail.com>
2021-07-19 18:21:19 +0900

    주석

---
+++ Python/08_Comment/1.py
@@ -0,0 +1,16 @@
+'''
+조건문 예제
+brooklyn
+2021
+'''
+
+#user input password
+input = 11
+real_brooklyn = 11
+#real_sydney = 22
+if real_brooklyn == input:
+    print("Hello! brooklyn")
+# elif real_sydney == input:
+#     print("Hello! sydney")
+else:
+    print("Who are you?")

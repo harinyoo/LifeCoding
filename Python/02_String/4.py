2d695f27ad0f251f754581d9372826c107f32319
harin yoo <u.halynn@gmail.com>
2021-07-19 15:44:59 +0900

    특수한 문자들

---
+++ Python/02_String/4.py
@@ -0,0 +1,8 @@
+print("harin's \"tutorials\"") #"를 특수기호가 아닌 문자로 해석해서 출력하기위해 \(escape)를 쓴다.
+#print("\")
+print("\\")
+print("Hello\nworld")
+print("Hello\tworld")
+print("Hello\t\tworld")
+print("\a") #\a : alert
+print('Hello\nworld')

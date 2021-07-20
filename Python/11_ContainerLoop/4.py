75a54e6c917a15b103f8f9f6aef567cbc0ac7dde
harin yoo <u.halynn@gmail.com>
2021-07-21 02:15:54 +0900

    로그인 애플리케이션에 투입

--- Python/11_ContainerLoop/4.py
+++ Python/11_ContainerLoop/4.py
@@ -1,5 +1,5 @@
 input_id = input("아이디를 입력해주세요.\n")
-members = ['brooklyn', 'sydney']
+members = ['brooklyn', 'sydney', 'victoria']
 for member in members:
     if member == input_id:
         print('Hello!, ' + member)
